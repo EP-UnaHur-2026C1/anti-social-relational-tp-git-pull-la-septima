b2ea1dd801570442f1bacf3a20943d3d13129501
Cris Ramirez <ramirezcris.cpr@gmail.com>
2026-05-12 14:20:53 -0300

    Construccion de las asosiaciones en base al DER funcionando

--- src/db/models/post_images.js
+++ src/db/models/post_images.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Post_Images.belongsTo(models.Post, { foreignKey : 'id_post', as : 'post'})
     }
   }
   Post_Images.init({
--- src/db/models/user.js
+++ src/db/models/user.js
@@ -10,7 +10,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      User.hasMany(models.Post,{foreignKey : 'user_nickname', as : 'posts'} ),
+      User.hasMany(models.Comment, {foreignKey : 'user_nickname', as : 'comments'})
     }
   }
   User.init({
