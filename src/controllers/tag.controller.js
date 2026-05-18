4c4a80cdcd256eb60312dc07cfdebff30447b626
Nicolas Blanco <nicolas.blanco@estudiantes.unahur.edu.ar>
2026-05-18 17:18:32 -0300

    feat: updateTag, fetAllTags y getTagById creados CRUD

--- src/controllers/tag.controller.js
+++ src/controllers/tag.controller.js
@@ -3,11 +3,11 @@ const { Tag } = require('../db/models')
 const createTag = async (req, res) => {
     try {
         const nombre = req.body
-        const tag = Tag.create(nombre)
-        res.status(201).json({message: "Tag creado exitosamente"})
-        return 
+        await Tag.create(nombre)
+        res.status(201).json({ message: "Tag creado exitosamente" })
+        return
     } catch (err) {
-        res.status(500).json({message: `${err}`})
+        res.status(500).json({ message: `${err}` })
         return
     }
 }
@@ -19,12 +19,50 @@ const deleteTag = async (req, res) => {
         res.status(200).json({message: `Tag eliminado exitosamente`})
         return
     } catch (err) {
-        res.status(500).json({message: `${err}`})
+        res.status(500).json({ message: `${err}` })
+        return
+    }
+}
+
+const updateTag = async (req, res) => {
+    try {
+        const { id } = req.params
+        const datosTag = req.body
+        await Tag.update(datosTag, { where: { id } })
+        res.status(200).json(datosTag)
+        return
+    } catch (err) {
+        res.status(500).json({ message: `${err}` })
+        return
+    }
+}
+
+const getAllTags = async (_, res) => {
+    try {
+        const tags = await Tag.findAll()
+        res.status(200).json(tags)
+        return
+    } catch (err) {
+        res.status(500).json({ message: `${err}` })
+        return
+    }
+}
+
+const getTagById = async (req, res) => {
+    try {
+        const { id } = req.params
+        const tag = await Tag.findByPk(id)
+        res.status(200).json(tag)
+    } catch (err) {
+        res.status(500).json({ message: `${err}` })
         return
     }
 }
 
 module.exports = {
     createTag,
-    deleteTag
+    deleteTag,
+    updateTag,
+    getAllTags,
+    getTagById
 }
--- src/routes/tag.routes.js
+++ src/routes/tag.routes.js
@@ -1,8 +1,11 @@
 const { Router } = require('express')
 const router = Router()
-const { createTag, deleteTag } = require('../controllers/tag.controller')
+const { createTag, deleteTag, updateTag, getAllTags, getTagById } = require('../controllers/tag.controller')
 
 router.post('/', createTag)
+router.get('/', getAllTags)
+router.get('/:id', getTagById)
+router.put('/:id', updateTag)
 router.delete('/:id', deleteTag)
 
 module.exports = router;
