eb6ffdf6b1cbab1ec58254c8bbd72084133ecdd0
Nicolas Blanco <nicolas.blanco@estudiantes.unahur.edu.ar>
2026-05-20 13:05:51 -0300

    feat: updateVisibility agregada

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,11 +1,12 @@
 const { Router } = require("express");
 const router = Router();
-const {createComment, getCommentsByPost, deleteComment, updateComment} = require('../controllers/comment.controller');
+const {createComment, getCommentsByPost, deleteComment, updateComment, updateVisibilityByMonth} = require('../controllers/comment.controller');
 
 router.post('/user/:id_user/post/:id_post', createComment);
 router.get('/post/:id_post', getCommentsByPost);
 router.delete('/delete/:id', deleteComment); 
 router.put('/update/:id', updateComment);   
+router.patch('/updateVisibility', updateVisibilityByMonth)
 
 module.exports = router;
     
