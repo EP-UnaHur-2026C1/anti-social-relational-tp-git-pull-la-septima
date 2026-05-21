e63d0afaa16393d85016036815f6e45a61d5902d
NicolasDondero <nico.donderox@gmail.com>
2026-05-21 20:58:47 -0300

    feat: creacion de middlewares genericos, de user y schema de user

--- src/routes/tag.routes.js
+++ src/routes/tag.routes.js
@@ -2,6 +2,7 @@ const { Router } = require('express')
 const router = Router()
 const { createTag, deleteTag, updateTag, getAllTags, getTagById } = require('../controllers/tag.controller')
 
+
 router.post('/', createTag)
 router.get('/', getAllTags)
 router.get('/:id', getTagById)
