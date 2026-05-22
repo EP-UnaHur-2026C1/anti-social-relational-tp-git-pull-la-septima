e62124671990dbd80e4f83fc1be18fdf1535d68f
Cris Ramirez <ramirezcris.cpr@gmail.com>
2026-05-22 14:55:54 -0300

    feat : validatePostByImageId, routes de post_images con middlewares

--- src/middlewares/post_images.middleware.js
+++ src/middlewares/post_images.middleware.js
@@ -4,7 +4,7 @@ const { postImagnesSchema } = require("../schemas/post_images.schema")
 
 const validateSchemaPostImages = validateSchema(postImagnesSchema)
 
-const validatePosImagestId = async(req, res, next) => {
+const validatePostImagestId = async(req, res, next) => {
         try{
             const {id_post} = req.params
             if(isNaN(id_post)||parseInt(id_post) <= 0){
@@ -23,7 +23,24 @@ const validatePosImagestId = async(req, res, next) => {
         next()
 }
 
+const validatePostByImageId = async (req, res, next) => {
+    try {
+        const {id_post, id_pi } = req.params
+
+        const validate = await Post_Images.findOne({ where : { id: id_pi } && { id_post : id_post }})
+
+        if(!validate) {
+            res.status(404).json({message: `La imagen con id ${id_pi} no fue encontrada en relacion con el post ${id_post}`})
+            return
+        }
+    } catch(err) {
+        res.status(500).json({message :`${err}`})
+        return
+    }
+}
+
 module.exports = {
     validateSchemaPostImages,
-    validatePosImagestId
+    validatePostImagestId,
+    validatePostByImageId
 }
--- src/routes/post_images.routes.js
+++ src/routes/post_images.routes.js
@@ -1,10 +1,16 @@
 const { Router } = require("express");
 const router = Router();
 const { createPostImage, getPostImages, getPostImageById, updatePostImage, deletePostImage } = require('../controllers/post_images.controller')
-const { validatePosImagestId, validateSchemaPostImages } = require('../middlewares/post_images.middleware')
-router.post('/user/:id/post/:id_post/images', createPostImage);
-router.get('/user/:id/post/:id_post/images', getPostImages);
-router.get('/user/:id/post/:id_post/images/:id_pi', getPostImageById);
-router.put('/user/:id/post/:id_post/images/:id_pi', updatePostImage);
-router.delete('/user/:id/post/:id_post/images/:id_pi', deletePostImage);
+const { validatePostImagestId, validateSchemaPostImages, validatePostByImageId } = require('../middlewares/post_images.middleware')
+const { validateUserId } = require('../middlewares/user.middleware')
+const { validatePostId, validatePostByUser } = require('../middlewares/post.middleware')
+
+const validateIdsAndPostByUser = [ validateUserId, validatePostId, validatePostByUser ]
+
+router.post('/user/:id/post/:id_post/images', validateSchemaPostImages, validateIdsAndPostByUser, createPostImage);
+router.get('/user/:id/post/:id_post/images', validateIdsAndPostByUser, getPostImages);
+router.get('/user/:id/post/:id_post/images/:id_pi', validateIdsAndPostByUser, validatePostImagestId, validatePostByImageId, getPostImageById);
+router.put('/user/:id/post/:id_post/images/:id_pi', validateSchemaPostImages, validateIdsAndPostByUser, validatePostImagestId, validatePostByImageId,  updatePostImage);
+router.delete('/user/:id/post/:id_post/images/:id_pi', validateIdsAndPostByUser, validatePostImagestId, validatePostByImageId, deletePostImage);
+
 module.exports = router;
