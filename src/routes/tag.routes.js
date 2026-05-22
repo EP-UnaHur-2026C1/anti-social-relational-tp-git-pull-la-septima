d1c9ca49adcb703b1baaa2e34c398859c0dd5700
Lucas Carrasco <lucasecarrasco1@gmail.com>
2026-05-21 21:12:14 -0300

    feat: tagMiddleware

--- src/routes/tag.routes.js
+++ src/routes/tag.routes.js
@@ -1,12 +1,12 @@
 const { Router } = require('express')
 const router = Router()
 const { createTag, deleteTag, updateTag, getAllTags, getTagById } = require('../controllers/tag.controller')
+const {validateTagId, validateSchemaTag} = require("../middlewares/tag.middleware")
 
-
-router.post('/', createTag)
+router.post('/', validateSchemaTag ,createTag)
 router.get('/', getAllTags)
-router.get('/:id', getTagById)
-router.put('/:id', updateTag)
-router.delete('/:id', deleteTag)
+router.get('/:id',validateTagId, getTagById)
+router.put('/:id', validateSchemaTag, validateTagId, updateTag)
+router.delete('/:id', validateTagId, deleteTag)
 
 module.exports = router;
