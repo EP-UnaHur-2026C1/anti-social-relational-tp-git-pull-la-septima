40f793ad31172686c187b4b589b9260c0487d0d0
Nicolas Blanco <nicolas.blanco@estudiantes.unahur.edu.ar>
2026-05-22 20:03:39 -0300

    feat: agregado validate en comment.routes.js

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,12 +1,13 @@
 const { Router } = require("express");
 const router = Router();
 const {createComment, getCommentsByPost, deleteComment, updateComment, updateVisibilityByMonth} = require('../controllers/comment.controller');
+const { validateCommentId, validateSchemaComment, validateSchemaUpdateVisibility, validateCommentUserId, validateCommentPostId } = require('../middlewares/comment.middleware')
 
-router.post('/user/:id_user/post/:id_post', createComment);
-router.get('/post/:id_post', getCommentsByPost);
-router.delete('/delete/:id', deleteComment); 
-router.put('/update/:id', updateComment);   
-router.patch('/updateVisibility', updateVisibilityByMonth)
+router.post('/user/:id_user/post/:id_post', validateSchemaComment, validateCommentUserId, validateCommentPostId, createComment);
+router.get('/post/:id_post', validateCommentPostId, getCommentsByPost);
+router.delete('/delete/:id', validateCommentId, deleteComment); 
+router.put('/update/:id', validateSchemaComment, validateCommentId, updateComment);   
+router.patch('/updateVisibility', validateSchemaUpdateVisibility, updateVisibilityByMonth)
 
 module.exports = router;
     
