8215574167fe35de634c7fd84fb80af129c9b99e
Nicolas Blanco <nicolas.blanco@estudiantes.unahur.edu.ar>
2026-05-29 11:21:07 -0300

    feat: fileFiltrer y validacion de tamaño de imagen

--- src/middlewares/upload.middleware.js
+++ src/middlewares/upload.middleware.js
@@ -1,5 +1,15 @@
 const multer = require("multer");
-const storage = multer.diskStorage({
+const storageMulter= multer.diskStorage({
     destination : (req, file, cb) => cb(null, "../../media"),
     filename : (req, file, cb) => cb(null, `${Date.now()}-${file.originalname}`)
 })
+
+const fileFilter = (req, file, cb) => {
+    
+    const ok = /jpeg|jpg|png|webp/.test(file.mimetype);
+    cb( ok ? null : new Error('Formato inválido', ok));
+}
+
+module.exports = multer({ storageMulter ,fileFilter, 
+    limits:{ fileSize: 5 * 1024 * 1024 }
+ })
