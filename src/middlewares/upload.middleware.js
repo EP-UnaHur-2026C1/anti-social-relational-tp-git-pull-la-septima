bf841d8d2ae7f16f1c30bb1c1f3ef0f7d848e795
NicolasDondero <nico.donderox@gmail.com>
2026-05-29 11:01:16 -0300

    feat: configurado multer en upload.middleware

--- src/middlewares/upload.middleware.js
+++ src/middlewares/upload.middleware.js
@@ -1,1 +1,5 @@
 const multer = require("multer");
+const storage = multer.diskStorage({
+    destination : (req, file, cb) => cb(null, "../../media"),
+    filename : (req, file, cb) => cb(null, `${Date.now()}-${file.originalname}`)
+})
